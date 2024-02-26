1bbe9240fc0698c110affba74a878804ebec47e9
Edu-20241 <demunoz05@gmail.com>
2024-02-26 07:58:03 -0500

    dfjskjn

---
+++ musica.py
@@ -0,0 +1,17 @@
+import random 
+class Instrumento:
+    def __init__(self,ins):
+        self.instrumento 
+        ins = [for i in ["Guitarra", "piano", "Saxofon", "Triangulo", "flauta", "clarinete", "trombon", "Maracas", "Arpa", "Bateria"]]
+        random.shuffl(ins)
+class Guitarra:
+class Piano:
+class Saxofon:
+class Triangulo:
+class flauta:
+class Clarinete:
+class Trombon:
+class Maracas:
+class Arpa:
+class Bateria:
+                 
